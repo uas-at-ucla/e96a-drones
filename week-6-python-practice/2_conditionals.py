081e63ec7383257274b7a5dc4fef5f5bdf012340
James Tseng <58379166+jtfa22@users.noreply.github.com>
2021-08-26 19:01:54 -0700

    Complete 3_loops

--- week-6-python-practice/2_conditionals.py
+++ week-6-python-practice/2_conditionals.py
@@ -5,9 +5,9 @@
 # Feel free to comment out your code for other Parts you’re not currently working on.
 # A quick way to comment out a lot of code is to select the lines and press Ctrl(Cmd) + /
 
-##
-## Part 1: isOdd
-##
+###
+### Part 1: isOdd
+###
 
 # create a function that has one input, num, called isOdd
 # the function returns a boolean as to whether or not num is odd
@@ -16,9 +16,9 @@
 
 # TODO: your code goes here
 
-##
-## Part 2: CHONK Chart
-##
+###
+### Part 2: CHONK Chart
+###
 
 # have the user enter a positive weight (pounds) of their (imaginary) cat
 # create a variable to store the rating of the input using this chart https://i.redd.it/am6cna8huen11.png (the large bold numbers are pounds)
@@ -29,13 +29,12 @@
 
 # TODO: your code goes here
 
-##
-## Part 3: coordinatePosition
-##
+###
+### Part 3: coordinatePosition
+###
 
 # create a function that has two inputs x, and y, called coordinatePosition
-# the function returns a a string 'N','S','E','W','NE','SE','NW','SW', or 'C'
-# based on the position of x and y relative to the origin (0,0) on a Cartesian Plane
+# the function returns a a string 'N','S','E','W','NE','SE','NW','SW', or 'C' based on the position of x and y relative to the origin (0,0) on a Cartesian Plane
 # the function returns 'C' if both x and y as ints both equal 0
 #   a.k.a. if x and y both round to 0, it's close enough to be counted as 'C' 
 # the function returns 'N','S','E', or 'W' if the corresponding x or y as int equal 0
--- week-6-python-practice/3_loops.py
+++ week-6-python-practice/3_loops.py
@@ -1,3 +1,69 @@
 ### E96A: Drones
 ### Week 6 Python Practice
 ### Loops
+
+# Feel free to comment out your code for other Parts you’re not currently working on.
+# A quick way to comment out a lot of code is to select the lines and press Ctrl(Cmd) + /
+
+###
+### Part 1: for a while
+###
+
+# using range(), create a for loop that prints out the even numbers sequentially from 10 to 2
+# tip: when using range that counts down, you need to specify the step
+
+# TODO: your code goes here
+
+# using a counter variable, create a while loop that prints out the even numbers sequentially from 10 to 2
+
+# TODO: your code goes here
+
+# as you can see, while both get the same job done, one approach (for) is better than the other
+# this is one of the principles of python, where there should just be one proper approach to handle a task
+
+###
+### Part 2: Triangle
+###
+
+# using for loops, print out this:
+#   * * * * *
+#   * * * *
+#   * * * 
+#   * *
+#   *
+# note: there is a space between each star
+# hint: think of the main (outer) for loop as each line, and the inner loop prints out the star loop many times
+# tip: use the arguments inside print() to help you out, such as the "end" argument
+
+# TODO: your code goes here
+
+###
+### Part 3: Collatz Conjecture
+###
+
+# consider a simple function:
+#   for a given number,
+#   if the number is even, divide it by two
+#   if the number is odd, triple it and add one
+# surprisingly, such as simple formula leads to some very interesting mathematical analysis
+# the Collatz Conjecture (which is still unproven!) states that this process will eventually reach the number 1, regardless of which positive integer is chosen initially
+
+# create a function called collatz that takes in one input (integer)
+# the function returns the result of the function on the input
+# tip: since the function might involve division, consider converting the return value to an int
+
+# TODO: your code goes here
+
+# now have the user enter a positive number (integer)
+# create a list with the user entered number
+# using a while loop, call the collatz function you have made and append the result to the list until the result is 1
+# once outside the loop, print the length and maximum value from the list
+# you can also print out the list if you're curious to see the progression
+# hint: use len() to get the length, max() for maximum value (and fyi, min() gets the smallest value)
+#   for example, print(len(list_var)) and print(max(list_var))
+
+# TODO: your code goes here
+
+###
+### End
+###
