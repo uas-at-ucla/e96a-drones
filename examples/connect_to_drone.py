f2b109fbbac539b105a87c4e1fc4d8e2080c5fc9
James Tseng <58379166+jtfa22@users.noreply.github.com>
2021-09-01 01:39:54 -0700

    Start Week 7 and examples

---
+++ examples/connect_to_drone.py
@@ -0,0 +1,22 @@
+### E96A: Drones
+### Week 7 Software Control
+### Connect to Drone
+
+import asyncio
+from mavsdk import System
+
+async def connectToDrone():
+    drone = System()
+    ## Radio Telemetry
+    # MacOS
+    await drone.connect(system_address="serial:///dev/tty.usbserial-0001") # check in Terminal
+    # Windows
+    await drone.connect(system_address="serial:///COM1") # check in Control Panel
+    # Mavlink Forward
+    await drone.connect(system_address="udp://:14445") # check in QGC
+
+    ## Wifi
+    # MacOS and Windows
+    await drone.connect(system_address="udp://:14445") # TODO
+    # Mavlink Forward
+    await drone.connect(system_address="udp://:14445") # check in QGC
---
+++ week-7-software-control/README.md
@@ -0,0 +1,4 @@
+# Welcome to MAVSDK!
+We've put together this assignment to help you get more familiar with MAVSDK and it's complicated structure of classes, functions, and use of `async`!
+
+## Getting started
